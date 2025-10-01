482cfd74f99e83ef0210a4643edb06c583d344ce
ranran-beib <254107060060@student.polinema.ac.id>
2025-10-01 20:48:12 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS05.java
+++ jobsheet5/ifCetakKRS05.java
@@ -8,12 +8,20 @@ public class ifCetakKRS05 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
+        // Versi IF-ELSE
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
         }
+
+        // Versi Ternary Operator
+        String hasil = (uktLunas) 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+        System.out.println("=== Versi Ternary Operator ===");
+        System.out.println(hasil);
 
         sc.close();
     }
