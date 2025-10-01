aa9c68140c6386f41f2b99041cbfead837d88a71
ranran-beib <254107060060@student.polinema.ac.id>
2025-10-01 20:38:02 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS05.java
+++ jobsheet5/ifCetakKRS05.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ifCetakKRSNoPresensi {
+public class ifCetakKRS05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
