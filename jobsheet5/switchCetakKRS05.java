86acc87f8440d85b033adaf623537735e82a8032
ranran-beib <254107060060@student.polinema.ac.id>
2025-10-01 20:37:23 +0700

    Percobaan 2

--- jobsheet5/switchCetakKRS05.java
+++ jobsheet5/switchCetakKRS05.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class switchCetakKRSNoPresensi {
+public class switchCetakKRS05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
